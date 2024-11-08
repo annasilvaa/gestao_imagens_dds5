0a28111e97492c814aac67c8f00c154008805fb6
ANNA JULYA DOS SANTOS DA SILVA <a.silva@labcetec.net>
2024-11-08 17:05:49 -0300

    Login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import GestaoImagem from './views/GestaoImagem';
+import TelaLogin from './views/TelaLogin';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<GestaoImagem />} />
+        <Route path='/login' element={<TelaLogin/>} />
       </Routes>
       </Router>
   );
--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,7 @@ import fileUpload from 'express-fileupload';
 import cors from 'cors';
 
 import {criarImagem, editarImagem, mostrarImagens, deletarImagem, mostrarUmaImagem, downloadImagem} from './controllers/ImagemController.js';
+import { criarUsuario, logarUsuario, mostrarUmUsuario, mostrarUsuario } from './controllers/UsuarioController.js';
 
 const app = express();
 const porta = 5000;
@@ -22,6 +23,16 @@ app.get('/imagem',mostrarImagens);
 app.put('/imagem/:id_imagem', mostrarUmaImagem)
 app.put('/imagem/:id_imagem', editarImagem)
 app.delete('/imagem/:id_imagem', deletarImagem)
+
+//CRUD usuario
+app.post ('/usuario', criarUsuario);
+app.get ('/usuario', mostrarUsuario);
+app.get ('/usuario/:id_usuario', mostrarUmUsuario);
+
+//Efetuar login
+app.post('/login', logarUsuario);
+
+
 
 app.listen(porta, ()=>{
 console.log (`API Rodando na porta ${porta}`)
