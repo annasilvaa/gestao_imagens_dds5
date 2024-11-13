4f5d7b77cc212ce3d9cebf3f1e2945e52b38af83
ANNA JULYA DOS SANTOS DA SILVA <a.silva@labcetec.net>
2024-11-13 16:12:17 -0300

    Implantando tipos de usuarios

--- client/src/views/GestaoImagem.jsx
+++ client/src/views/GestaoImagem.jsx
@@ -1,38 +1,52 @@
+
 import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 function GestaoImagem() {
     const [imagens, setImagens] = useState([]);
     const [imagem, setImagem] = useState(null);
     const [descricao, setDescricao] = useState('');
+    const [idUsuario, setIdUsuario] = useState('');
+    const navigate = useNavigate();
+    const [login, setLogin] = useState('');
+    const [funcao, setFuncao] = useState('');
+
 
     useEffect(() => {
-        carregarImagem()
+        carregarImagens();
+        if (idUsuario === '') {
+            try {
+                const id_usuario = localStorage.getItem('id_usuario');
+                if (!id_usuario) {
+                    alert('Efetue Login');
+                    navigate('/login');
+                } else {
+                    setIdUsuario(id_usuario);
+                    getNomeFuncao(id_usuario);
+                }
+            } catch (error) {
+                console.log('');
+            }
+        }
     }, []);
 
-    async function cadastrarImagem(params) {
-        const formData = new FormData();
-        formData.append('descricao', descricao);
-        formData.append('imagem', imagem);
-
+    async function getNomeFuncao(id_usuario) {
         try {
-            const resposta = await fetch('http://localhost:5000/imagem', {
-                method: 'POST',
-                body: formData
-            })
-
-            if (!resposta) {
-                throw new Error('Erro ao cadastrar imagem');
-            } else {
-                carregarImagem();
+            const resposta = await fetch(`http://localhost:5000/usuario/${id_usuario}`);
+            const dados = await resposta.json();
+            if (dados) {
+                console.log(dados);
+                setLogin(dados.login);
+                setFuncao(dados.funcao);
             }
         } catch (error) {
-            throw new Error('Erro ao cadastrar imagem', error);
+            console.log(error);
         }
     }
 
-    async function carregarImagem() {
+    async function carregarImagens() {
         try {
-            const resposta = await fetch('http://localhost:5000/imagem/', {
+            const resposta = await fetch('http://localhost:5000/imagem', {
                 method: 'GET',
                 headers: {
                     'Content-Type': 'application/json'
@@ -45,15 +59,37 @@ function GestaoImagem() {
             const consulta = await resposta.json();
             setImagens(consulta);
         } catch (error) {
-            console.log('Erro ao buscar imagens', error)
+            console.log('Erro ao buscar imagens', error);
         }
     }
+
+    async function cadastrarImagem() {
+        const formData = new FormData();
+        formData.append('descricao', descricao);
+        formData.append('imagem', imagem);
+
+        try {
+            const resposta = await fetch('http://localhost:5000/imagem', {
+                method: 'POST',
+                body: formData
+            });
+
+            if (!resposta) {
+                throw new Error('Erro ao cadastrar imagem');
+            } else {
+                carregarImagens();
+            }
+        } catch (error) {
+            throw new Error('Erro ao cadastrar imagem', error);
+        }
+    }
+
     async function deletarImagem(id_imagem) {
         try {
             const resposta = await fetch(`http://localhost:5000/imagem/${id_imagem}`, {
                 method: 'DELETE',
                 headers: {
-                    'Content-type': 'application/json'
+                    'Content-Type': 'application/json'
                 }
             });
 
@@ -64,12 +100,17 @@ function GestaoImagem() {
                 setImagens(imagens.filter(imagem => imagem.id_imagem !== id_imagem));
             }
         } catch (error) {
-            throw new Error('Erro ao deletar imagem', error)
+            throw new Error('Erro ao deletar imagem', error);
         }
     }
+
+    function logout() {
+        localStorage.removeItem('id_usuario');
+        navigate('/login');
+    }
+
     return (
         <>
-
             <div>
                 <nav className='container'>
                     <span>Logo</span>
@@ -77,43 +118,33 @@ function GestaoImagem() {
                     <ul>
                         <li>Inicio</li>
                     </ul>
+                    <button className='btn btn-danger' onClick={logout}>Logout</button>
                 </nav>
             </div>
             <div className='container'>
-                <h1 className='text-center'> Gestão Imagens</h1>
+                <h1 className='text-center'>Gestão Imagens</h1>
+                <h2>{`Bem Vindo ${login}`}</h2>
+                <h3>{funcao === 'ADM' && 'Você é administrador'}</h3>
                 <div>
-                    <h2>Cadastrar imagem</h2>
-                    <label htmlFor="">Descrição</label>
-                    <input
-                        className='form-control'
-                        type="text"
-                        value={descricao}
-                        onChange={e => (setDescricao(e.target.value))} />
+                    <h2>Cadastrar Imagem</h2>
+                    <label htmlFor="">Descrições</label>
+                    <input className='form-control' type="text" value={descricao} onChange={e => (setDescricao(e.target.value))} />
                     <label htmlFor="">Imagem</label>
-                    <input
-                        className='form-control'
-                        type="file"
-                        onChange={e => (setImagem(e.target.files[0]))}
-                        name="" id="" />
-                    <button className='btn btn-sucess mt-2' onClick={cadastrarImagem}> Cadastrar</button>
+                    <input className='form-control' type="file" onChange={e => (setImagem(e.target.files[0]))} />
+                    <button className='btn btn-success' onClick={cadastrarImagem}>Cadastrar</button>
                 </div>
                 <div className='row mt-2'>
                     {imagens.map((imagem) => (
                         <div className='col-md-3' key={imagem.id_imagem}>
-                            <img className='img-thumbnail'
-                                src={`http://localhost:5000/public/${imagem.caminho}`}
-                                alt={imagem.descricao}
-                            />
+                            <img className='img-thumbnail' src={`http://localhost:5000/public/${imagem.caminho}`} alt="" />
                             <div className='mt-2'>
-                                <button className='btn btn-primary'> Editar</button>
-                                <button
-                                    className='btn btn-danger float-end'
-                                    onClick={() => deletarImagem(imagem.id_imagem)}>Deletar</button>
+                                <button className='btn btn-primary'>Editar</button>
+                                <button className='btn btn-danger float-end' onClick={() => deletarImagem(imagem.id_imagem)}>Deletar</button>
                             </div>
                         </div>
-                    ))}
+                    ))};
                 </div>
-            </div >
+            </div>
         </>
     )
 }
